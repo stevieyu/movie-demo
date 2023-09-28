e8ffa85273ad42411c1fd6f97179787cde47c847
Stevie <yzm629@sina.cn>
2023-09-29 01:31:01 +0800

    -

--- src/plugins/urql.js
+++ src/plugins/urql.js
@@ -1,6 +1,24 @@
-import urql, { cacheExchange, fetchExchange } from '@urql/vue';
+import urql, { fetchExchange } from '@urql/vue';
+import { offlineExchange } from '@urql/exchange-graphcache';
+import { makeDefaultStorage } from '@urql/exchange-graphcache/default-storage';
 
 //https://formidable.com/open-source/urql/docs/basics/vue/
+
+const storage = makeDefaultStorage({
+  idbName: 'graphcache-v3', // The name of the IndexedDB database
+  maxAge: 7, // The maximum age of the persisted data in days
+});
+
+const cache = offlineExchange({
+  // schema,
+  storage,
+  // updates: {
+  //   /* ... */
+  // },
+  // optimistic: {
+  //   /* ... */
+  // },
+});
 export default {
   install(app){
     app.use(urql, {
@@ -10,7 +28,7 @@ export default {
           'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTQ4ODE0MDAsImlzcyI6ImdyYWZiYXNlIiwiYXVkIjoiMDFIQUZDVkhXNDYyNEVKRUNZODNCTUUwNVciLCJqdGkiOiIwMUhBRkNWSFk5UEQzSDFaMDJUWVdWMzU5SCIsImVudiI6InByb2R1Y3Rpb24iLCJwdXJwb3NlIjoicHJvamVjdC1hcGkta2V5In0.DXgg-JbnRjV9oeLoK9VGKqoAl_C-rzfyzXVrdU63ezM'
         },
       },
-      exchanges: [cacheExchange, fetchExchange]
+      exchanges: [cache, fetchExchange]
     });
   }
 }
