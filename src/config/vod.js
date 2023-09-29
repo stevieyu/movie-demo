245975adc295f43683ee57e56147156ba9f14516
Stevie <yzm629@sina.cn>
2023-09-29 08:07:12 +0800

    -

--- src/config/vod.js
+++ src/config/vod.js
@@ -5,7 +5,8 @@ export const url = [
   'https://bfzyapi-com-et9m5eezqmu0.curlhub.io/api.php/provide/vod/',
   'https://by-bfzyapi-com-et9m5eezqmu0.curlhub.io/api.php/provide/vod/',
   'https://jiguang.pro/api.php/provide/vod/',// 开头文字广告,待去掉
+  'https://suoniapi.com/api.php/provide/vod/',
   'https://cj.lziapi.com/api.php/provide/vod/',
   'https://api.haiwaikan.com/v1/vod',
   'https://kuaikan-api.com/api.php/provide/vod/'
-][0]
+][5]
