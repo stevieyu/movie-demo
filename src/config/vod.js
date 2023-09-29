34668b50dc6f59336a526b2814d432acee2eeec2
Stevie <yzm629@sina.cn>
2023-09-29 08:17:11 +0800

    -

--- src/config/vod.js
+++ src/config/vod.js
@@ -1,12 +1,12 @@
 
 export const url = [
   'https://faas-sgp1-18bc02ac.doserverless.co/api/v1/web/fn-41e9df6e-4d4b-4032-8fb2-e91907859969/default/cors-php/bfzyapi.com/api.php/provide/vod/',
-  'https://by.bfzyapi.com/api.php/provide/vod/',
   'https://bfzyapi-com-et9m5eezqmu0.curlhub.io/api.php/provide/vod/',
-  'https://by-bfzyapi-com-et9m5eezqmu0.curlhub.io/api.php/provide/vod/',
-  'https://jiguang.pro/api.php/provide/vod/',// 开头文字广告,待去掉
-  'https://suoniapi.com/api.php/provide/vod/',
-  'https://cj.lziapi.com/api.php/provide/vod/',
+  'http://by.bfzyapi.com/api.php/provide/vod/',
+  'http://cj.lziapi.com/api.php/provide/vod/',
+  'http://suoniapi.com/api.php/provide/vod/',
+  'http://cj.ffzyapi.com/api.php/provide/vod/',
+  'http://jiguang.pro/api.php/provide/vod/',// 开头文字广告
   'https://api.haiwaikan.com/v1/vod',
   'https://kuaikan-api.com/api.php/provide/vod/'
-][5]
+][4]
