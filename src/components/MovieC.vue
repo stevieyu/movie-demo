81058f659627a64c8d86da31828afd935149a640
Stevie <yzm629@sina.cn>
2023-10-10 13:53:59 +0800

    -

--- src/components/MovieC.vue
+++ src/components/MovieC.vue
@@ -67,8 +67,14 @@ watch(video, (val) => {
 }, {immediate: true})
 
 const playUrl = computed(() => (video.value?.playUrl?.match(/([^$#]+)\$(http[^$#]+m3u8)/g) || [])
-  .map((i) => {
-    let [name, src] = i.split('$')
+  .map(i => i.split('$'))
+  .reduce((unique, item) => {
+    if (!unique.some(i => i[1] === item[1])) {
+      unique.push(item);
+    }
+    return unique;
+  }, [])
+  .map(([name, src]) => {
     src = 'https://faas-sgp1-18bc02ac.doserverless.co/api/v1/web/fn-41e9df6e-4d4b-4032-8fb2-e91907859969/default/cors-php' + src.replace(/https?:\//, '');
     // src = 'https://closely-dynamic-quetzal.edgecompute.app/hono.dgjx.workers.dev' + src.replace(/https?:\//, '');
     return {
