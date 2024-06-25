7ec18572dc73cd6721ebc329d3c807bae4bcf811
stevieyu <yzm629@sina.cn>
2024-06-25 08:59:27 +0800

    --

--- src/plugins/urql.js
+++ src/plugins/urql.js
@@ -22,16 +22,16 @@ const cache = offlineExchange({
 export default {
 	install(app) {
 		app.use(urql, {
-			// url: 'https://yszy-dgjx.grafbase.app/graphql',
-			url: "https://dgjx-hasura.stellate.sh",
+			url: 'https://yszy-dgjx.grafbase.app/graphql',
+			// url: "https://dgjx-hasura.stellate.sh",
 			fetchOptions: {
 				headers: {
 					accept: "application/json",
-					// 'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTQ4ODE0MDAsImlzcyI6ImdyYWZiYXNlIiwiYXVkIjoiMDFIQUZDVkhXNDYyNEVKRUNZODNCTUUwNVciLCJqdGkiOiIwMUhBRkNWSFk5UEQzSDFaMDJUWVdWMzU5SCIsImVudiI6InByb2R1Y3Rpb24iLCJwdXJwb3NlIjoicHJvamVjdC1hcGkta2V5In0.DXgg-JbnRjV9oeLoK9VGKqoAl_C-rzfyzXVrdU63ezM'
+					'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTQ4ODE0MDAsImlzcyI6ImdyYWZiYXNlIiwiYXVkIjoiMDFIQUZDVkhXNDYyNEVKRUNZODNCTUUwNVciLCJqdGkiOiIwMUhBRkNWSFk5UEQzSDFaMDJUWVdWMzU5SCIsImVudiI6InByb2R1Y3Rpb24iLCJwdXJwb3NlIjoicHJvamVjdC1hcGkta2V5In0.DXgg-JbnRjV9oeLoK9VGKqoAl_C-rzfyzXVrdU63ezM'
 				},
 			},
 			exchanges: [
-				// cache, 
+				cache, 
 				fetchExchange
 			],
 		});
