cfb909ce3b768f34454c0e023f7db01f1c1bba95
Your Name <you@example.com>
2024-08-22 09:50:33 +0000

    -

--- src/config/vod.js
+++ src/config/vod.js
@@ -6,8 +6,7 @@ export const url = [
 	"http://cj.ffzyapi.com/api.php/provide/vod/",
 	"http://suoniapi.com/api.php/provide/vod/",
 	"http://kuakezy1.com/api.php/provide/vod/", // 随机微广告,无法屏蔽,video.js代理播放不能任意进度,hls.js 可以播放
-	"http://jiguang.pro/api.php/provide/vod/", // 随机微广告
 	"https://api.haiwaikan.com/v1/vod",
 	"https://kuaikan-api.com/api.php/provide/vod/",
 	"https://xsd.sdzyapi.com/api.php/provide/vod/",
-][2];
+][9];
